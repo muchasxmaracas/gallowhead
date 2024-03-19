adab2b01147f19241258e03c60221be07fabaef8
muchasxmaracas <sandromarton95@gmail.com>
2024-03-19 01:15:20 +0100

    update: set up baseline design

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have the 'gallowhead' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('gallowhead');
+    expect(app.title).toEqual('Gallowhead');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, gallowhead');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Gallowhead Band');
   });
 });
