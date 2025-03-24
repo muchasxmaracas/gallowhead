e8eb457185d6a039b2821cef128e61facdc52d8e
muchasxmaracas <sandromarton95@gmail.com>
2025-03-24 03:00:42 +0100

    chore: remove localhost:3000 and insert test.api.gallowhead as url

--- frontend/src/app/dates/dates.component.ts
+++ frontend/src/app/dates/dates.component.ts
@@ -49,7 +49,7 @@ export class DatesComponent implements OnInit {
     }
 
     fetchConcertData() {
-        const url = `http://localhost:3000/api/sheet-data?sheetId=${this.sheetId}&range=${this.range}`;
+        const url = `http://test.api.gallowhead.com/api/sheet-data?sheetId=${this.sheetId}&range=${this.range}`;
 
         this.http.get<SheetResponse>(url).subscribe({
             next: (response: SheetResponse) => this.processSheetData(response),
